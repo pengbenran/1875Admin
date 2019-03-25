c4788e6235c08fdee7168dbea4d0f702161c6e2e
pengbenran <810648616@qq.com>
2019-03-25 11:22:41 +0800

    pbr

---
+++ src/views/membership/membershipLevel/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="membershipManagerment">
+    这是会员等级界面
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+     
+      }
+    },
+    mounted () {
+   
+    },
+    methods: {
+  
+
+     
+    }
+  }
+</script>
+
+<style lang="scss">
+  
+</style>
---
+++ src/views/membership/recommenderLevel/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="membershipManagerment">
+    这是推荐师管理列表
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+     
+      }
+    },
+    mounted () {
+   
+    },
+    methods: {
+  
+
+     
+    }
+  }
+</script>
+
+<style lang="scss">
+  
+</style>
